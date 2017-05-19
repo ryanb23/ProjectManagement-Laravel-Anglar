6c34532e54468409b24d4b3ac9ca914f4f3e62f3
Ryan <ryanbrucen1023@outlook.com>
2017-05-19 20:44:24 +0200

    app/config update

--- config/app.php
+++ config/app.php
@@ -158,7 +158,6 @@ return [
         Dingo\Api\Provider\LaravelServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ResponseMacroServiceProvider::class,
-        
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         LaravelAngular\Generators\LaravelServiceProvider::class,
