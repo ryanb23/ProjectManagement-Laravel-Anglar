a3cd03af19e63e733b7e0ed80e7dd4a78fd1ac95
Ryan <ryanbrucen1023@outlook.com>
2017-05-22 04:09:24 +0200

    NG Scroll-bottom directive add

---
+++ angular/directives/chat/scroll-to-bottom.component.js
@@ -0,0 +1,21 @@
+scrollToBottom.$inject = ['$timeout', '$window']
+function scrollToBottom ($timeout,$window) {
+  return {
+        scope: {
+            scrollToBottom: "="
+        },
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            scope.$watchCollection('scrollToBottom', function(newVal) {
+                if (newVal) {
+                    $timeout(function() {
+                        element[0].scrollTop =  element[0].scrollHeight;
+                    }, 0);
+                }
+
+            });
+        }
+    };
+}
+
+export const ScrollToBottomComponent = scrollToBottom
--- angular/index.directives.js
+++ angular/index.directives.js
@@ -1,6 +1,8 @@
 import { RouteBodyClassComponent } from './directives/route-bodyclass/route-bodyclass.component'
 import { PasswordVerifyClassComponent } from './directives/password-verify/password-verify.component'
+import { ScrollToBottomComponent } from './directives/chat/scroll-to-bottom.component'
 
 angular.module('app.components')
   .directive('routeBodyclass', RouteBodyClassComponent)
   .directive('passwordVerify', PasswordVerifyClassComponent)
+  .directive('scrollToBottom', ScrollToBottomComponent)
