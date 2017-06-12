860a3cfb72679eaeb0e2d538df67cc7ee61fd4d1
Ryan <ryanbrucen1023@outlook.com>
2017-06-12 11:07:48 +0200

    Ng, Department component update

--- angular/app/components/department-management/department-management.component.js
+++ angular/app/components/department-management/department-management.component.js
@@ -9,55 +9,79 @@ class DepartmentManagementController {
     let that = this
     this.table = $('#tableDepartments')
     this.addTable = $('#addDepartmentModal')
-
+    this.addTable_dj = null
     this.options = {
         "sDom": "<'table-responsive't><'row'<p i>>",
         "destroy": true,
+        "bSort": false,
         "scrollCollapse": true,
         "oLanguage": {
             "sLengthMenu": "_MENU_ ",
             "sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
         },
-        "iDisplayLength": 5
+        "iDisplayLength": 10
     };
     this.departments = [];
     this.p_department_sel = {};
-    this.p_departments = [{id:'0',name:'---',des:'',p_dep:''}];
+    this.p_departments = [];
 
-    this.newDepartment = {name:'a',active: '1', des:'a' ,p_dep:'0'}
+    this.newDepartment = {name:'a',active: true, des:'a' ,p_dep:'0'}
+
+    $scope.$on('initDataTable', function(ngRepeatFinishedEvent) {
+      if($.fn.DataTable.isDataTable( '#tableDepartments' ))
+        that.addTable_dj.fnDestroy()
+      that.addTable_dj = that.table.dataTable(that.options)
+    });
+  }
+  InitValues(dep_list){
+    if($.fn.DataTable.isDataTable( '#tableDepartments' ))
+      this.addTable_dj.fnDestroy()
+    this.departments = dep_list;
+    this.p_departments = [{id:'0',name:'---',des:'',p_dep:''}];
+    for(var i=0; i<dep_list.length; i++)
+    {
+      var item = dep_list[i];
+      this.p_departments.push({id:item.id,name:item.name,des:item.description})
+    }
   }
 
   getDepartment(){
     this.departmentRoute.get('index').then((response) => {
         var dep_list = response.plain().data;
-        this.table.dataTable();
-        this.departments = dep_list;
-        console.log(this.departments)
-        for(var i=0; i<dep_list.length; i++)
-        {
-          var item = dep_list[i];
-          this.p_departments.push({id:item.id,name:item.name,des:item.description})
-        }
+        this.InitValues(dep_list)
     })
   }
   filter(event) {
     this.table.dataTable().fnFilter($(event.currentTarget).val());
   }
-  showModal(){
-    this.addTable.modal('show');
-  }
-  addNewUser() {
-    var selected_dep_id = this.p_department_sel.selected.id;
+
+  addNewDepartment() {
+    let selected_dep_id = this.p_department_sel.selected.id;
     this.newDepartment.p_dep = selected_dep_id
     this.departmentRoute.all("new-department").post(this.newDepartment).then((response) => {
-        this.getDepartment()
+        var dep_list = response.plain().data;
+        this.InitValues(dep_list)
     })
     this.addTable.modal('hide');
+  }
+
+  editDepartment(params){
+    let type = params.type;
+    let id = params.id;
+    console.log(id)
+  }
+
+  removeDepartment(params){
+
+  }
+  // Modal Functions
+  showModal(){
+    this.addTable.modal('show')
   }
   hideModal() {
       this.addTable.modal('hide');
   }
-
+  
   trustAsHtml(value) {
       return this.$sce.trustAsHtml(value);
   }
