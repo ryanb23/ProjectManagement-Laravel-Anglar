f169f804c8cc16c1937b6ffb0152d447d597ac87
Ryan <ryanbrucen1023@outlook.com>
2017-06-17 17:02:37 +0200

    NG. project management create

--- angular/app/components/projects/projects.component.js
+++ angular/app/components/projects/projects.component.js
@@ -1,7 +1,27 @@
 class ProjectsController {
-  constructor ($scope) {
+  constructor (API, $scope, $sce, $compile, $filter) {
     'ngInject'
-    $scope.test = 'test';
+
+    let that = this
+    this.$sce = $sce;
+    this.$scope = $scope;
+    this.$compile = $compile;
+    this.$filter = $filter;
+    this.API = API;
+
+    this.departmentRoute = API.all('departments');
+    this.departments = [];
+  }
+
+  getDepartment() {
+      this.API.all('departments').get('index').then((response) => {
+          var dep_list = response.plain().data
+          this.departments = dep_list;
+      })
+  }
+
+  $onInit() {
+      this.getDepartment()
   }
 }
 
