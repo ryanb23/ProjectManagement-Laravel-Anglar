330b2428a2f98669b65f5208b3e33e52c09eb839
Ryan <ryanbrucen1023@outlook.com>
2017-06-20 15:32:29 +0200

    App. Traits update

---
+++ app/Http/Traits/CustomTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait CustomTrait
+{
+    function time_elapsed_string ($time)
+    {
+        $time = strtotime($time);
+        $time = time() - $time; // to get the time since that moment
+        $time = ($time<1)? 1 : $time;
+        $tokens = array (
+            31536000 => 'year',
+            2592000 => 'month',
+            604800 => 'week',
+            86400 => 'day',
+            3600 => 'hour',
+            60 => 'minute',
+            1 => 'second'
+        );
+
+        foreach ($tokens as $unit => $text) {
+            if ($time < $unit) continue;
+            $numberOfUnits = floor($time / $unit);
+            return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':'').' ago';
+        }
+    }
+
+}
--- app/Http/Traits/FileTrait.php
+++ app/Http/Traits/FileTrait.php
@@ -5,6 +5,42 @@ namespace App\Http\Traits;
 trait FileTrait
 {
     public function fileUpload($project_id, $files){
-        echo 1;die;
+        $upload_dir = env('UPLOAD_DIR');
+        $result = array();
+        foreach($files as $key => $file)
+        {
+            $filename = $file['filename'];
+            $file_des = $upload_dir.'/'.$project_id.'/'.$filename;
+
+            if (!file_exists($upload_dir.'/'.$project_id.'/')) {
+                mkdir($upload_dir.'/'.$project_id.'/', 0777, true);
+            }
+            if (rename($upload_dir.'/tmp/'.$filename, $file_des))
+            {
+                $result[] = $file;
+            }
+        }
+        return $result;
+    }
+
+    public function fileToTmp($file){
+        $upload_dir = env('UPLOAD_DIR');
+        $result = array();
+        $path_parts = pathinfo($file['file']['name']);
+        $filename = $path_parts['filename'].'_'.time().'.'.$path_parts['extension'];
+        $file_des = $upload_dir.'/tmp/'.$filename;
+
+        if (!file_exists($upload_dir.'/tmp/')) {
+            mkdir($upload_dir.'/tmp/', 0777, true);
+        }
+        if (move_uploaded_file($file['file']['tmp_name'], $file_des))
+        {
+            $result = array(
+                'type'=> $file['file']['type'],
+                'filename'=> $filename,
+                'org_filename'=> $file['file']['name']
+            );
+        }
+        return $result;
     }
 }
