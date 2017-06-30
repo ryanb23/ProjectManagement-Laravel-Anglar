5b8c48772c822ca9bf0283a7ceba821a231429ae
Ryan <ryanbrucen1023@outlook.com>
2017-06-30 07:29:20 +0200

    ng. profile(other) create

---
+++ angular/app/components/user-profile-other/user-profile-other.component.js
@@ -0,0 +1,42 @@
+class UserProfileOtherController {
+    constructor(API, $scope, $state, $stateParams, ContextService, $anchorScroll) {
+        'ngInject'
+
+        let that = this
+        this.userId = parseInt($stateParams.userId)
+
+        this.userInfo = null
+        this.projects = []
+        this.$state = $state
+        this.ContextService = ContextService
+
+        this.projectRoute = API.all('projects')
+        this.userRoute = API.all('users')
+    }
+
+    detailView(project){
+        this.$state.go('app.projects.view', {projectId: project.id})
+    }
+
+    getProjectList(){
+        this.projectRoute.get('user-projects',{'id':this.userInfo.id}).then((response) => {
+            this.projects = response.plain().data
+        })
+    }
+
+    $onInit() {
+        let that = this
+        this.userRoute.get('show',{'id':that.userId}).then((response) => {
+            that.userInfo = response.plain().data
+            that.getProjectList();
+        })
+    }
+
+}
+
+export const UserProfileOtherComponent = {
+    templateUrl: './views/app/components/user-profile-other/user-profile-other.component.html',
+    controller: UserProfileOtherController,
+    controllerAs: 'vm',
+    bindings: {}
+}
