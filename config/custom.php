97d2fccf9ef39f2d0964b20f9479296d84639a82
Ryan <ryanbrucen1023@outlook.com>
2017-07-04 03:27:42 +0200

    App. Notification Config and Lang file create

---
+++ config/custom.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel Custom
+     |--------------------------------------------------------------------------
+     */
+     /*** Notification Types ****/
+     'notification_new_post' => 1,
+     'notification_project_upvote' => 2,
+     'notification_project_comment' => 3,
+     'notification_project_popular' => 4,
+     'notification_roject_approved' => 5,
+     'notification_project_dismissed' => 6
+];
---
+++ resources/lang/en/notification.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notification Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'notification_new_post' => 'new post.',
+    'notification_project_upvote' => 'project upvote',
+    'notification_project_comment' => 'project comment',
+    'notification_project_popular' => 'project upvote',
+    'notification_roject_approved' => 'project approved',
+    'notification_project_dismissed' => 'project dismissed'
+];
