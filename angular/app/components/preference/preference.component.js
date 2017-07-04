81fca3c9892210b944b1b897b2e4b71bcbf2fae1
Ryan <ryanbrucen1023@outlook.com>
2017-07-04 03:31:58 +0200

    Ng. Component preference update

--- angular/app/components/preference/preference.component.js
+++ angular/app/components/preference/preference.component.js
@@ -1,16 +1,51 @@
 class PreferenceController {
-  constructor () {
-    'ngInject'
+    constructor(API, $scope, $sce, $compile, $filter) {
+        'ngInject'
 
-  //
-  }
+        let that = this
 
-  $onInit () {}
+        this.userRoute = API.all('users');
+        this.preference = {
+            "id": null,
+            "post": 0 ,
+            "upvote": 0 ,
+            "comment": 0 ,
+            "popular": 0 ,
+            "approved": 0 ,
+            "dismissed": 0
+        };
+    }
+
+    getPreference(){
+        this.userRoute.get('user-setting').then((response) => {
+            let result = response.plain().data;
+            if(result.length)
+            {
+                let data = result[0];
+                this.preference.id          = data.id;
+                this.preference.post        = (data.post == '1')? true: false
+                this.preference.upvote      = (data.upvote == '1')? true: false
+                this.preference.comment     = (data.comment == '1')? true: false
+                this.preference.popular     = (data.popular == '1')? true: false
+                this.preference.approved    = (data.approved == '1')? true: false
+                this.preference.dismissed   = (data.dismissed == '1')? true: false
+            }
+        })
+    }
+
+    save(){
+        this.userRoute.all('user-setting').post({data:this.preference}).then((response) => {
+
+        })
+    }
+    $onInit() {
+        this.getPreference()
+    }
 }
 
 export const PreferenceComponent = {
-  templateUrl: './views/app/components/preference/preference.component.html',
-  controller: PreferenceController,
-  controllerAs: 'vm',
-  bindings: {}
+    templateUrl: './views/app/components/preference/preference.component.html',
+    controller: PreferenceController,
+    controllerAs: 'vm',
+    bindings: {}
 }
