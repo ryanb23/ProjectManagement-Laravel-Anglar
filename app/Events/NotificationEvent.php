43bf57b80e07bf687888ec60571efb0cbe89ae87
Ryan <ryanbrucen1023@outlook.com>
2017-07-04 03:30:16 +0200

    App. Event notification Create

---
+++ app/Events/NotificationEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Events\Event;
+use App\User;
+use Auth;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class NotificationEvent extends Event implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $message, $to_id;
+    public $user;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+    public function __construct($message,$to_id)
+    {
+        //
+        $this->message = $message;
+        $user = Auth::user();
+        $this->to_id = $to_id;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return ['notification-channel'];
+    }
+}
