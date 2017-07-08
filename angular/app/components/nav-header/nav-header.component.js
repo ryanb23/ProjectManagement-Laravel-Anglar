0b3d3c59d89388974e9712b5f3feda2e8290c523
Ryan <ryanbrucen1023@outlook.com>
2017-07-08 16:39:52 +0200

    Ng. Nav-header notification update

--- angular/app/components/nav-header/nav-header.component.js
+++ angular/app/components/nav-header/nav-header.component.js
@@ -1,14 +1,33 @@
 class NavHeaderController {
-  constructor ($rootScope, ContextService) {
+  constructor ($rootScope, $scope, ContextService) {
     'ngInject'
 
     let navHeader = this
-    this.$rootscope = $rootScope;
+    this.$scope = $scope;
+    this.$rootScope = $rootScope;
+    this.unreadMessage = [];
+    this.unreadMessageTotal = 0;
+
+    this.notificationRoute = API.all('users');
+
+    function  updateUnreadMessage(event, args) {
+        navHeader.unreadMessageTotal ++;
+        navHeader.$scope.$apply()
+    }
+
+    this.$rootScope.$on("newNotification", updateUnreadMessage);
+    var eventFunc = {
+        updateUnreadMessage: updateUnreadMessage
+    }
+
+    angular.extend(navHeader, eventFunc);
+
     ContextService.me(function (data) {
         if(data != null)
             navHeader.userData = data
     })
   }
+
   showSearchOverlay(){
     this.$rootscope.$broadcast('toggleSearchOverlay', {
         show: true
