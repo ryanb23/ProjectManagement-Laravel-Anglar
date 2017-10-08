fe7630bbfcef0ddf97a62aade6983199b09a6ccd
Ryan <ryanbrucen1023@outlook.com>
2017-10-09 00:17:57 +0800

    App. update notification controller

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,11 +9,13 @@ use Bican\Roles\Models\Permission;
 use Bican\Roles\Models\Role;
 use Hash;
 use Input;
+use \Config;
 use Validator;
 use App\Http\Requests;
 
 use App\User;
 use App\Models\Message;
+use App\Models\Notification;
 
 class NotificationController extends Controller
 {
@@ -30,6 +32,42 @@ class NotificationController extends Controller
         return response()->success($totalNotificaitonCnt);
     }
 
+    public function getNotifications(){
+        $result = self::userNotifications();
+        return response()->success($result);
+    }
+    public function postUpdateNotifications(Request $request)
+    {
+        $result = Notification::where(array(
+            'sender_id' => $request['user']['id'],
+            'notification_type' => $request['type'],
+            'resource_id' => $request['resource_id'],
+            'is_read' => 0
+        ))->update(['is_read' => 1]);
+
+        $result = self::userNotifications();
+        return response()->success($result);
+    }
+
+    private function userNotifications(){
+        $user = Auth::user();
+        $user_id = $user->id;
+        $notifications = Notification::with(array('sender' => function($query){
+                $query->select(['id','name','avatar','firstname','lastname']);
+            }))->where('to_id',$user_id)->where('is_read',0)->get()->toArray();
+
+        $result = array_map(function($item){
+            return array(
+                'type' => $item['notification_type'],
+                'user' => $item['sender'],
+                'resource_id' => $item['resource_id'],
+                'message' => Config::get("notificaitonMessages.{$item['notification_type']}"),
+                'updateTime' => $item['updated_at']
+            );
+        }, $notifications);
+        return $result;
+    }
+
     private function getMessageNotificationCount($user_id){
         $result = Message::where('to_id',$user_id)->where('is_read',0)->selectRaw('count(*) as cnt')->get('cnt');
         return $result;
@@ -38,11 +76,11 @@ class NotificationController extends Controller
     public function getChatNotificationList(){
         $user = Auth::user();
         $user_id = $user->id;
-        $result = Message::where('to_id',$user_id)->where('is_read',0)->selectRaw('count(*) as cnt,user_id')->groupby('user_id')->get()->toArray();
-        foreach($result as $key=>$value)
+        $db_result = Message::where('to_id',$user_id)->where('is_read',0)->selectRaw('count(*) as cnt,user_id')->groupby('user_id')->get()->toArray();
+        $result = [];
+        foreach($db_result as $key=>$value)
         {
             $result[$value['user_id']] = $value['cnt'];
-            unset($result[$key]);
         }
         return response()->success($result);
     }
