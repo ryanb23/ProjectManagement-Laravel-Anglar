bdaef36dfb2369cbac548b1b70974b032d008a10
Ryan <ryanbrucen1023@outlook.com>
2017-10-19 20:17:19 +0800

    NG. contact page update

--- angular/app/components/organization/organization.component.js
+++ angular/app/components/organization/organization.component.js
@@ -13,26 +13,81 @@ class OrganizationController {
     this.departmentRoute = API.all('departments');
     this.userRoute = API.all('users');
 
-    this.departments = [];
-    this.depSel = null;
+    this.searchName = '';
+
+    this.departments = {};
+    this.departments.list = [];
+    this.departments.sel = [];
+    this.departments.setting = {
+        showCheckAll: false,
+        showUncheckAll: false,
+        smartButtonMaxItems: 1,
+        smartButtonTextConverter: function(itemText, originalItem) { return itemText;}
+    }
+    function depSelect(){
+        that.filterUsers();
+    }
+    this.departments.event_setting ={
+        onSelectionChanged: depSelect
+    }
+
+    angular.extend(that, this.departments.event_setting);
+
+    this.permissions = {};
+    this.permissions.list = [];
+    this.permissions.sel = [];
+    this.permissions.setting = {
+        showCheckAll: false,
+        showUncheckAll: false,
+        smartButtonMaxItems: 1,
+        smartButtonTextConverter: function(itemText, originalItem) { return itemText;}
+    }
+    function pemSelect(){
+        that.filterUsers();
+    }
+    this.permissions.event_setting ={
+        onSelectionChanged: pemSelect
+    }
+
+    angular.extend(that, this.departments.event_setting);
 
     this.uesrs = []
   }
 
-  getDepartment() {
-      this.departmentRoute.get('department-tree-with-users').then((response) => {
-        var dep_list = response.plain().data
-        this.departments = dep_list['treeData'];
-        this.depSel  = this.departments[0]['id']
-        this.getUsers(this.departments[0])
+  trustAsHtml(value) {
+      return this.$sce.trustAsHtml(value);
+  };
+
+  getPermission() {
+      this.userRoute.get('roles').then((response) => {
+        var permission_list = response.plain().data.roles;
+        this.permissions.list = permission_list.map(function(item){
+            return {id: item.id, label: item.name};
+        });
       })
   }
 
-  getUsers(dep){
-      this.depSel  = dep['id']
-      this.userRoute.get('department-users',{'id':dep['id']}).then((response) => {
+  getDepartment() {
+      this.departmentRoute.get('index').then((response) => {
+        var dep_list = response.plain().data
+        this.departments.list = dep_list.map(function(item){
+            return {id: item.id, label: item.name};
+        });
+        this.filterUsers();
+      })
+  }
+
+  filterUsers(deps){
+      let depIds = this.departments.sel.map(function(item){return item['id']}).join(",");
+      let roleIds = this.permissions.sel.map(function(item){return item['id']}).join(",");
+      this.userRoute.get('filter-users',{departments: depIds, roles: roleIds}).then((response) => {
           this.users = response.plain().data
       });
+  }
+
+  filterByName(user)
+  {
+      return user.name.toLowerCase().includes(this.searchName.toLowerCase());
   }
 
   viewProfile(user)
@@ -40,8 +95,10 @@ class OrganizationController {
       let $state = this.$state
       $state.go('app.user.other-profile',{userId:user.id})
   }
+
   $onInit() {
       this.getDepartment();
+      this.getPermission();
   }
 }
 
