8cfce8e9e91c27064d75c7a93a9d0b08bcdef2a3
Ryan <ryanbrucen1023@outlook.com>
2017-10-22 02:20:35 +0800

    NG. other user profile page update

--- angular/app/components/user-profile-other/user-profile-other.component.js
+++ angular/app/components/user-profile-other/user-profile-other.component.js
@@ -18,12 +18,30 @@ class UserProfileOtherController {
         this.$state.go('app.projects.view', {projectId: project.id})
     }
 
+    viewProfile(user)
+    {
+        let $state = this.$state
+        $state.go('app.user.other-profile',{userId:user.id})
+    }
+
     getProjectList(){
         this.projectRoute.get('user-projects',{'id':this.userInfo.id}).then((response) => {
             this.projects = response.plain().data
         })
     }
-    
+
+    getUserPoint(){
+        this.userRoute.get('user-point',{'id':this.userInfo.id}).then((response) => {
+            this.userInfo['point'] = response.plain().data;
+        })
+    }
+
+    getUserActivities(){
+        this.userRoute.get('user-activities',{'id':this.userInfo.id}).then((response) => {
+            this.userActivities = response.plain().data
+        })
+    }
+
     getImageUrl(project_id, filename){
         return 'pro_imgs/' + project_id+ '/' + filename
     }
@@ -33,6 +51,8 @@ class UserProfileOtherController {
         this.userRoute.get('show',{'id':that.userId}).then((response) => {
             that.userInfo = response.plain().data
             that.getProjectList();
+            that.getUserActivities();
+            that.getUserPoint();
         })
     }
 
